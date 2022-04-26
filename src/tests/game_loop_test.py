ed71514ec6139de8f873345c070c438251b60515
Niklas Nygård <niklas.nygard01@gmail.com>
2022-04-26 12:15:08 +0300

    added a new test, testing coins

--- src/tests/game_loop_test.py
+++ src/tests/game_loop_test.py
@@ -7,6 +7,7 @@ class Player(object):
 
     def __init__(self):
         self.rect = pygame.Rect(32, 32, 16, 16)
+
     def move(self, dx, dy):
 
         if dx != 0:
@@ -18,13 +19,23 @@ class Player(object):
 
         self.rect.x += dx
         self.rect.y += dy
-        
+
+
 
 class TestGame1(unittest.TestCase):
         def setUp(self):
             self.peli = GameLoop()
+            self.sum_coin = 0
+            self.coins = [pygame.Rect(550, 14, 19, 19)]
             
         def test_aloituspaikka(self):
             pelaaja = Player()
             self.assertEqual((pelaaja.rect.x,pelaaja.rect.y), (32,32))
+
+        def test_raha(self):
+            pelaaja = Player()
+            for coin in self.coins:
+                if pelaaja.rect.colliderect(coin):
+                    self.assertEqual(self.sum_coin, +10)
+
 
